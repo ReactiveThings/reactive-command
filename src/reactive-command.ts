a001d70f4bd2b0deefffd571b1ef058da88140a3
Pawel Milka <skubekk@hotmail.com>
2019-05-26 16:02:42 +0200

    remove subs

--- src/reactive-command.ts
+++ src/reactive-command.ts
@@ -1,13 +1,4 @@
-
-import {
-  Subject,
-  throwError,
-  of,
-  from,
-  combineLatest,
-  defer,
-  Observable
-} from 'rxjs';
+import { Subject, throwError, of, from, combineLatest, defer, Observable } from 'rxjs'
 import {
   catchError,
   startWith,
@@ -20,122 +11,124 @@ import {
   tap,
   finalize,
   publishLast
-} from 'rxjs/operators';
-import { Command } from './command';
+} from 'rxjs/operators'
+import { Command } from './command'
 import { ExecutionInfo } from './internal/execution-info'
-import { ExecutionDemarcation } from './internal/execution-demarcation';
+import { ExecutionDemarcation } from './internal/execution-demarcation'
 
 export class ReactiveCommand<TParam, TResult> implements Command<TParam, TResult> {
-  private readonly _execute: (param: TParam | undefined) => Observable<TResult>;
+  private readonly _execute: (param: TParam | undefined) => Observable<TResult>
 
-  private readonly isExecuting$: Observable<boolean>;
-  private readonly canExecute$: Observable<boolean>;
-  public readonly results: Observable<TResult>;
-  private readonly exceptions$: Subject<any> = new Subject<any>();
-  private readonly executionInfo$ = new Subject<ExecutionInfo<TResult>>();
+  private readonly isExecuting$: Observable<boolean>
+  private readonly canExecute$: Observable<boolean>
+  public readonly results: Observable<TResult>
+  private readonly exceptions$: Subject<any> = new Subject<any>()
+  private readonly executionInfo$ = new Subject<ExecutionInfo<TResult>>()
 
-  constructor(execute: (param: TParam | undefined) => Observable<TResult>, canExecute: Observable<boolean> = of(true)) {
-    this._execute = execute;
+  constructor(
+    execute: (param: TParam | undefined) => Observable<TResult>,
+    canExecute: Observable<boolean> = of(true)
+  ) {
+    this._execute = execute
 
-    this.isExecuting$ = this.createIsExecuting$();
-    this.canExecute$ = this.createCanExecute$(canExecute);
-    this.results = this.createResult$();
-    this.canExecute$.subscribe();
+    this.isExecuting$ = this.createIsExecuting$()
+    this.canExecute$ = this.createCanExecute$(canExecute)
+    this.results = this.createResult$()
   }
 
-
-  public static createFromObservable<TParam, TResult>(execute: (param?: TParam) => Observable<TResult>, canExecute?: Observable<boolean>): Command<TParam, TResult> {
-    return new ReactiveCommand<TParam, TResult>(execute, canExecute);
+  public static createFromObservable<TParam, TResult>(
+    execute: (param?: TParam) => Observable<TResult>,
+    canExecute?: Observable<boolean>
+  ): Command<TParam, TResult> {
+    return new ReactiveCommand<TParam, TResult>(execute, canExecute)
   }
 
-  public static createFromPromise<TParam, TResult>(execute: (param?: TParam) => Promise<TResult>, canExecute?: Observable<boolean>): Command<TParam, TResult> {
-    return new ReactiveCommand<TParam, TResult>((param) => from(execute(param)), canExecute);
+  public static createFromPromise<TParam, TResult>(
+    execute: (param?: TParam) => Promise<TResult>,
+    canExecute?: Observable<boolean>
+  ): Command<TParam, TResult> {
+    return new ReactiveCommand<TParam, TResult>(param => from(execute(param)), canExecute)
   }
 
   public static create(canExecute?: Observable<boolean>): Command<any, any> {
-    return ReactiveCommand.createFromObservable<any, any>(p => of(p), canExecute);
+    return ReactiveCommand.createFromObservable<any, any>(p => of(p), canExecute)
   }
 
   get canExecute(): Observable<boolean> {
-    return this.canExecute$;
+    return this.canExecute$
   }
 
   get isExecuting(): Observable<boolean> {
-    return this.isExecuting$;
+    return this.isExecuting$
   }
 
   get errors(): Observable<any> {
-    return this.exceptions$;
+    return this.exceptions$
   }
 
   public execute(parameter?: TParam): Observable<any> {
-    return defer(
-      () => {
-        this.executionInfo$.next(ExecutionInfo.CreateBegin<TResult>());
-        return this._execute(parameter);
-      }).pipe(
-        tap(result => this.executionInfo$.next(ExecutionInfo.CreateResult(result))),
-        catchError(ex => {
-            this.exceptions$.next(ex);
-            return throwError(ex);
-          }),
-        finalize(() => this.executionInfo$.next(ExecutionInfo.CreateEnd<TResult>())),
-        publishLast(),
-        refCount()
-      );
+    return defer(() => {
+      this.executionInfo$.next(ExecutionInfo.CreateBegin<TResult>())
+      return this._execute(parameter)
+    }).pipe(
+      tap(result => this.executionInfo$.next(ExecutionInfo.CreateResult(result))),
+      catchError(ex => {
+        this.exceptions$.next(ex)
+        return throwError(ex)
+      }),
+      finalize(() => this.executionInfo$.next(ExecutionInfo.CreateEnd<TResult>())),
+      publishLast(),
+      refCount()
+    )
   }
 
   public executeAsync(parameter?: TParam): Promise<TResult> {
-    return this.execute(parameter).toPromise();
+    return this.execute(parameter).toPromise()
   }
 
-
   private createResult$() {
-    return this
-      .executionInfo$.pipe(
-        filter((x: ExecutionInfo<TResult>) => x.Demarcation === ExecutionDemarcation.Result),
-        map((x: ExecutionInfo<TResult>) => x.Result as TResult)
-      );
+    return this.executionInfo$.pipe(
+      filter((x: ExecutionInfo<TResult>) => x.Demarcation === ExecutionDemarcation.Result),
+      map((x: ExecutionInfo<TResult>) => x.Result as TResult)
+    )
   }
 
   private createIsExecuting$() {
     return this.executionInfo$.pipe(
       scan((acc: number, next: ExecutionInfo<TResult>) => {
         if (next.Demarcation === ExecutionDemarcation.Begin) {
-          return acc + 1;
+          return acc + 1
         }
 
         if (next.Demarcation === ExecutionDemarcation.End) {
-          return acc - 1;
+          return acc - 1
         }
-        return acc;
+        return acc
       }, 0),
       map((inFlightCount: number) => inFlightCount > 0),
       startWith(false),
       distinctUntilChanged(),
       publishReplay(1),
       refCount()
-    );
+    )
   }
 
   private createCanExecute$(canExecute: Observable<boolean>) {
     canExecute = canExecute.pipe(
       catchError(ex => {
-        this.exceptions$.next(ex);
-        return of(false);
+        this.exceptions$.next(ex)
+        return of(false)
       }),
       startWith(false)
     )
 
-    const canExecuteFunc = (canEx: boolean, isEx: boolean) => canEx && !isEx;
+    const canExecuteFunc = (canEx: boolean, isEx: boolean) => canEx && !isEx
 
-    return combineLatest(canExecute, this.isExecuting$)
-      .pipe(
-        map(x => canExecuteFunc(...x)),
-        distinctUntilChanged(),
-        publishReplay(1),
-        refCount()
-      );
+    return combineLatest(canExecute, this.isExecuting$).pipe(
+      map(x => canExecuteFunc(...x)),
+      distinctUntilChanged(),
+      publishReplay(1),
+      refCount()
+    )
   }
-
 }
